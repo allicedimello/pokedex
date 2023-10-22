6e91e879ba3fcccc9b8feeb347c6d7b26fb90dcf
AM <allicedimello@disroot.org>
2023-10-22 02:45:45 -0300

    Convertendo o modelo da pokeApi pro nosso modelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,24 @@
 const pokeApi = {}
 
+function convertPokemonModel(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokemonModel)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
@@ -14,12 +31,3 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonDetails) => pokemonDetails)
     }
-
-// Promise.all([
-//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/4')
-// ]).then((results) => {
-//     console.log(results)
-// })
